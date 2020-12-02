7291e0d6a3747a81aacd33b00477f775db94cb73
Shafee <shafeelinks@gmail.com>
2020-12-01 21:27:46 -0500

    tested axios to get geocode GET

--- src/js/API/gmaps.js
+++ src/js/API/gmaps.js
@@ -1,15 +1,15 @@
-// importing zomato.js
-// export to index.js
+// // importing zomato.js
+// // export to index.js
 
-// Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
-script.defer = true;
+// // Create the script tag, set the appropriate attributes
+// var script = document.createElement('script');
+// script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+// script.defer = true;
 
-// Attach your callback function to the `window` object
-window.initMap = function () {
-    // JS API is loaded and available
-};
+// // Attach your callback function to the `window` object
+// window.initMap = function () {
+//     // JS API is loaded and available
+// };
 
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
+// // Append the 'script' element to 'head'
+// document.head.appendChild(script);
---
+++ src/js/test.js
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+
+if (navigator.geolocation) {
+    console.log('Geolocation is supported!');
+}
+else {
+    console.log('Geolocation is not supported for this Browser/OS version yet.');
+}
+
+let ZOMATO_KEY = process.env.ZOMATO_KEY;
+let ZOMATO_URL = process.env.ZOMATO_URL;
+let GMAPS_KEY = process.env.GMAPS_KEY;
+
+navigator.geolocation.getCurrentPosition(giveLocation, error);
+
+function giveLocation(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+
+
+    async function getData() {
+        try {
+            const config = {
+                headers: {
+                    Accept: "application/json",
+                    "user-key": `${ZOMATO_KEY}`,
+                }
+            };
+            // GETTING GEOCODE DATA
+            const geocode = await axios.get(`${ZOMATO_URL}geocode?lat=${latitude}&lon=${longitude}`, config);
+            // RESTAURANT ID FROM GEOCODE
+            let restaurant_id = geocode.data.nearby_restaurants[0].restaurant.R.res_id;
+            // GETTING CUISINE DATA
+            // const cuisine = await axios.get(`${ZOMATO_URL}cuisines?lat=${latitude}&lon=${longitude}`, config);
+            // GETTING REVIEWS DATA
+            const reviews = await axios.get(`${ZOMATO_URL}reviews?res_id=${restaurant_id}`, config);
+
+            console.log(geocode, reviews);
+
+            for (let item of geocode.data.nearby_restaurants) {
+                console.log(item.restaurant.name);
+                console.log("Longitude: ", item.restaurant.location.longitude);
+                console.log("Latitude:", item.restaurant.location.latitude);
+            }
+
+        } catch (e) {
+            console.log('error', e);
+        }
+    }
+
+    getData();
+}
+
+
+
+function error(message) {
+    console.log('user declined location access');
+}
