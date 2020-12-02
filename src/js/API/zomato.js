fc9a1a52d86ad2022431284276b3e9855ab8b831
Shafee <shafeelinks@gmail.com>
2020-12-01 19:02:56 -0500

    pushing to shaf-dev

--- src/js/API/zomato.js
+++ src/js/API/zomato.js
@@ -9,8 +9,8 @@ const ZOMATO_URL = process.env.ZOMATO_URL;
 const ZOMATO_KEY = process.env.ZOMATO_KEY;
 
 // TEMPORARY PLACEHOLDERS FOR LAT AND LONG
-let longitude = -73.8566;
-let latitude = 40.8383;
+let longitude = -73.9571;
+let latitude = 40.7081;
 
 
 async function getData() {
@@ -22,13 +22,15 @@ async function getData() {
             }
         };
         // GETTING GEOCODE DATA
-        const geocode = await axios.get(`${ZOMATO_URL}api/v2.1/geocode?lat=${latitude}&lon=${longitude}`, config);
+        const geocode = await axios.get(`${ZOMATO_URL}geocode?lat=${latitude}&lon=${longitude}`, config);
         // RESTAURANT ID FROM GEOCODE
         let restaurant_id = geocode.data.nearby_restaurants[0].restaurant.R.res_id;
         // GETTING CUISINE DATA
-        const cuisine = await axios.get(`${ZOMATO_URL}api/v2.1/cuisines?lat=${latitude}&lon=${longitude}`, config);
+        const cuisine = await axios.get(`${ZOMATO_URL}cuisines?lat=${latitude}&lon=${longitude}`, config);
         // GETTING REVIEWS DATA
-        const reviews = await axios.get(`${ZOMATO_URL}api/v2.1/reviews?res_id=${restaurant_id}`, config);
+        const reviews = await axios.get(`${ZOMATO_URL}reviews?res_id=${restaurant_id}`, config);
+
+
 
         console.log(geocode, cuisine, reviews);
     } catch (e) {
