05e57c0202a15b302ee1a992df25ca3596e48af0
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-12-07 15:06:17 -0500

    aesthetic changes1

--- src/js/API/gmaps.js
+++ src/js/API/gmaps.js
@@ -1,23 +1,23 @@
 export function showSearchMarkers(arr, position) {
-    const image =
-        "https://raw.githubusercontent.com/Avixph/-Munch-Map-Redux/developer/src/images/logos/flag.png";
-    let map;
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: position,
-        zoom: 10,
-    });
+  const image =
+    "https://raw.githubusercontent.com/Avixph/-Munch-Map-Redux/d9f5320ac16d436d1f0acf174b0cdb7071651b88/src/images/icons/MM-Icon.svg";
+  let map;
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: position,
+    zoom: 10,
+  });
 
-    new google.maps.Marker({
-        position: position,
-        map,
-        title: "munch map!",
-    });
+  new google.maps.Marker({
+    position: position,
+    map,
+    title: "munch map!",
+  });
 
-    for (let item of arr) {
-        const marker = new google.maps.Marker({
-            position: item.ResCoordinates,
-            map,
-            icon: image,
-        });
-    }
+  for (let item of arr) {
+    const marker = new google.maps.Marker({
+      position: item.ResCoordinates,
+      map,
+      icon: image,
+    });
+  }
 }
