f55c230b60652d26ded5067d599936697b135e0b
Shafee <shafeelinks@gmail.com>
2020-12-06 20:55:00 -0500

    pushing search changes

--- src/js/API/zomato-gmap.js
+++ src/js/API/zomato-gmap.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { restaurantNearby } from '../nearbyResults.js';
+import { restaurantSearch } from '../searchResults.js';
 
 if (navigator.geolocation) {
   console.log("Geolocation is supported!");
@@ -8,12 +10,13 @@ if (navigator.geolocation) {
 
 let ZOMATO_KEY = process.env.ZOMATO_KEY;
 let ZOMATO_URL = process.env.ZOMATO_URL;
+let gMapUrl = process.env.GMAP_URL;
+let gMapKey = process.env.GMAP_KEY;
 
 // GEOLOCATION CALL
 navigator.geolocation.getCurrentPosition(giveLocation, error);
 
 // SEARCH
-
 const cardsec = document.querySelector('.card-section');
 
 cardsec.onscroll = function () {
@@ -41,6 +44,7 @@ function giveLocation(position) {
   // ASYNC FUNCTION START FOR API CALLS
   async function getData() {
     try {
+
       const config = {
         headers: {
           Accept: "application/json",
@@ -55,10 +59,10 @@ function giveLocation(position) {
       );
 
       // GETTING SEARCH RESULTS FOR ZOMATO - SORTED BY RATING
-      const search = await axios.get(`https://developers.zomato.com/api/v2.1/search?q=indian&count=20&lat=40.8383&lon=-73.8566&sort=rating`, config);
-      console.log(search);
+      const searchInput = document.querySelector('.search__bar');
+      const search = await axios.get(`https://developers.zomato.com/api/v2.1/search?q=${searchInput.value}&count=20&lat=${latitude}&lon=${longitude}&sort=rating`, config);
 
-      console.log(geocode);
+      console.log(search);
 
       // DISPLAY WHAT AREA THE USER IS IN
       const subzone = `${geocode.data.popularity.subzone}`;
@@ -81,7 +85,10 @@ function giveLocation(position) {
       }
 
       const allRestaurants = {};
+      let searchResults = {};
+
       // CHANGE NUMBER TO DOLLAR SIGNS FOR PRICE RANGE
+
       for (let item of geocode.data.nearby_restaurants) {
         if (typeof item.restaurant.price_range === "number") {
           let numOfTimes = item.restaurant.price_range;
@@ -109,59 +116,92 @@ function giveLocation(position) {
         };
       }
 
+
+      for (let item of search.data.restaurants) {
+        if (typeof item.restaurant.price_range === "number") {
+          let numOfTimes = item.restaurant.price_range;
+          item.restaurant.price_range = "";
+          for (let i = 0; i < numOfTimes; i++) {
+            item.restaurant.price_range += "$";
+          }
+        }
+
+
+        // OBJECT THAT CONTAINS ALL OF OUR DATA
+        searchResults[item.restaurant.name] = {
+          ResCoordinates: {
+            lat: Number(item.restaurant.location.latitude),
+            lng: Number(item.restaurant.location.longitude),
+          },
+
+          RestaurantName: item.restaurant.name,
+          Score: `${item.restaurant.user_rating.aggregate_rating}`,
+          ReviewText: `${item.restaurant.user_rating.rating_text}`,
+          Cuisine: item.restaurant.cuisines,
+          AverageCost: `${item.restaurant.average_cost_for_two}`,
+          PriceRange: `${item.restaurant.price_range}`,
+          FeaturedImg: item.restaurant.featured_image,
+          Location: `${item.restaurant.location.address}`,
+        };
+      }
+
       let restaurantValues = Object.values(allRestaurants);
+      let searchRestaurantValues = Object.values(searchResults);
+
+      console.log(searchRestaurantValues);
 
       // FUNCTION THAT RETURNS A NEW CARD WITH RESTAURANT INFO
-      let restaurant = ({
-        RestaurantName,
-        Score,
-        ReviewText,
-        Cuisine,
-        AverageCost,
-        PriceRange,
-        FeaturedImg,
-        Location,
-      }) => {
-        return `
-        <div class="col-lg-10 col-md-12 col-sm-12 card-container">
-            <h1 class="text-center res-name"><span class="info">${RestaurantName}</span></h1>
-            <div class="d-flex align-items-center row">
-                <div class="col-lg-6 col-md-12 col-sm-12">
-                    <div class="restaurant-container">
-                        <div class="col-12 card-info">
-                            <ul>
-                                <li>Cuisine: <span class="info">${Cuisine}</span></li>
-                                <li>Price range: <span class="info money">${PriceRange}</span></li>
-                                <li>Average cost for two: <span class="info">${Number(AverageCost) ? `$${AverageCost}` : `N/A`}</span></li>
-                                <li>Location: <span class="info">${Location}</span></li>
-                                <li>Score: <span class="info">${Number(Score) ? `${Score} / 5.0` : `N/A`}</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-12 col-sm-12">
-                    <div class="d-flex align-items-center food-container">
-                        <div class="col-12 text-center food-pic">
-                  
-                            <img src="${FeaturedImg ? FeaturedImg : "https://cdn.discordapp.com/attachments/661356197768855554/785260477022208030/404-img.png"}" alt="" /> 
-                            <h3><span class="info review">"${ReviewText}"</span></h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-      };
-
       let cardSection = document.querySelector(".card-section");
 
-
       // APPENDING NEW RESTAURANT CARDS TO OUR DOM
-      for (let item of restaurantValues) {
-        let resDiv = document.createElement("div");
-        resDiv.classList.add("d-flex", "justify-content-center");
-        resDiv.innerHTML = restaurant(item);
-        cardSection.append(resDiv);
+      function populateNearbyRestaurants() {
+        const cardContainer = document.querySelectorAll('.card-container');
+        for (let div of cardContainer) {
+          div.remove();
+        }
+        for (let item of restaurantValues) {
+          let resDiv = document.createElement("div");
+          resDiv.classList.add("d-flex", "justify-content-center");
+          resDiv.innerHTML = restaurantNearby(item);
+          cardSection.append(resDiv);
+        }
       }
+
+      populateNearbyRestaurants();
+
+      function populateRestaurant() {
+        for (let item of searchRestaurantValues) {
+          let resDiv = document.createElement("div");
+          resDiv.classList.add("d-flex", "justify-content-center");
+          resDiv.innerHTML = restaurantSearch(item);
+          cardSection.append(resDiv);
+        }
+      }
+
+      //  ADD SEARCH RESULT ITEMS HERE
+      const searchForm = document.querySelector('.search');
+
+      searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        searchInput.addEventListener('keyup', (e) => {
+
+          const cardContainer = document.querySelectorAll('.card-container');
+          for (let div of cardContainer) {
+            div.remove();
+          }
+
+          if (e.keyCode === 13) {
+            const city = `${geocode.data.location.city_name}`;
+            let area = document.querySelector(".subzone");
+            area.innerText = city;
+
+            populateRestaurant();
+          }
+        });
+      });
+
+
+
       // DISPLAYING GMAPS JS API
       // const script = document.createElement("script");
       // script.src = `${gMapUrl}js?key=${gMapKey}&callback=initMap`;
